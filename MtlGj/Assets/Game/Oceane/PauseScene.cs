ac67c22ddbbb1b67e18b4471d357dd9a8734373a
LilOceane <57155432+LilOceane@users.noreply.github.com>
2020-02-01 14:21:28 -0500

    sf

--- MtlGj/Assets/Game/Oceane/PauseScene.cs
+++ MtlGj/Assets/Game/Oceane/PauseScene.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void pauseSceneTrue(){
+                Time.timeScale = 0.0f;
+                  //DontDestroyOnLoad(transform.PauseGameLevel);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void pauseSceneFalse(){
+                Time.timeScale = 1.0f;
+                 // SceneManager.LoadScene(newGameLevel);
     }
 }
