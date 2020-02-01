185221f9e6a35bb7d665eb090ae7977c8423c544
yussefSD <43423160+yussefSD@users.noreply.github.com>
2020-02-01 01:54:41 -0500

    Functionning Shooting Turret (no health)
    :) <3

--- MtlGj/Assets/ShootingTower.cs
+++ MtlGj/Assets/ShootingTower.cs
@@ -5,24 +5,65 @@ using UnityEngine;
 public class ShootingTower : MonoBehaviour
 {
     GameObject[] objectArray;
+    GameObject closestObject;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    Transform target;
+    public float bulletForce;
+
     float distance;
+    List<float> distanceList;
+
+    float timer;
+    public int waitingTime;
+    public float movingDelay;
 
     // Update is called once per frame
     void Update()
     {
-        searchGameObjects("tag");
+        closestObject = FindNearestEnemy();
+        target = closestObject.transform.Find("Witch");
+
+        timer += Time.deltaTime;
+
+        if (timer > waitingTime && timer < movingDelay) {
+           
+        }
+        else{ Shoot(); timer = 0; }
     }
 
-    void searchGameObjects(string tag) 
+    GameObject FindNearestEnemy() 
     {
-        if (objectArray == null)
-            objectArray = GameObject.FindGameObjectsWithTag(tag);
+        objectArray = GameObject.FindGameObjectsWithTag("Enemy");
 
-        foreach (GameObject tagged in objectArray)  
-        {
-            distance = Vector3.Distance(tagged.transform.position, this.transform.position);
-            Debug.Log("Distance: " + distance);
+        float minDistance = 0;
+        int count = 0;
+
+        foreach (GameObject enemyItem in objectArray) {
+            float dist = Vector3.Distance(enemyItem.transform.position, firePoint.position);
+            if (count == 0) {
+                minDistance = dist;
+                closestObject = enemyItem;
+                count++;
+            }
+            else {
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    closestObject= enemyItem;
+                }
+            }
         }
+        return closestObject;
+    }
 
+    void Shoot() 
+    {
+        Vector2 vector = target.position - firePoint.position;
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(vector * bulletForce, ForceMode2D.Impulse);
     }
 }
