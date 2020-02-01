70ee32fe4288d7576b3869b98e434b24fc3e4a37
yussefSD <43423160+yussefSD@users.noreply.github.com>
2020-01-31 23:10:58 -0500

    r6s6tyr
    gtg

---
+++ MtlGj/Assets/ShootingTower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTower : MonoBehaviour
+{
+    GameObject[] objectArray;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void searchGameObjects(string tag) 
+    {
+        //if (objectArray == null)
+
+        //foreach (GameObject respawn in respawns)  
+        //{
+
+        //}
+
+    }
+}
