180a59416ff0cb6c6dbc9ae08c1365a6f50af363
LilOceane <57155432+LilOceane@users.noreply.github.com>
2020-02-01 12:44:29 -0500

    x

---
+++ MtlGj/Assets/Game/Oceane/beginEnding.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class beginEnding : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void SwitchScene(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+        
+       // gameObject.GetComponent<AudioSource>().Play();
+    }
+    public void quit() {
+
+        Debug.Log("Quit the game");
+		Application.Quit();
+	}
+}
