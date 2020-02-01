3b426246db746733cbc78aecb9468eabaed4d967
yussefSD <43423160+yussefSD@users.noreply.github.com>
2020-02-01 10:40:18 -0500

    Update TowerBuilderController.cs
    111

--- MtlGj/Assets/Game/Avatar/TowerBuilderController.cs
+++ MtlGj/Assets/Game/Avatar/TowerBuilderController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 using UnityEngine.Tilemaps;
 using Pathfinding = NesScripts.Controls.PathFind;
+using UnityEngine.UI;
 
 namespace MTLGJ
 {
@@ -18,6 +19,8 @@ namespace MTLGJ
         [SerializeField]
         private float _buildRange = 2f;
 
+        bool isMenuActive;
+        public Canvas turretMenu = false;
 
         IsometricCharacterRenderer isoRenderer;
 
@@ -84,36 +87,42 @@ namespace MTLGJ
 
             if (Input.GetButtonDown("Fire1"))
             {
-                //isoRenderer.dir
-
-                //Vector2 playerPos = avatar.transform.position;
-                //Vector2 playerDirection = avatar.transform.forward * 5;
-                //Quaternion playerRotation = avatar.transform.rotation;
-                //float spawnDistance = 500;
-                //Vector2 spawnPos = playerPos + playerDirection;
-                //Vector3Int plpl = new Vector3Int((int)playerPos.x, (int)playerPos.y, 3);
-
-                var curr = Level.Instance.Tilemap.GetTile(
-                    front.FromWorldToCellPosition());
-
-                if (curr != null && ((GGJTile)curr).ID == TileID.Building)
-                    return;
-
-                if (curr != null && ((GGJTile)curr).ID == TileID.Character)
-                    return;
-
-                if (curr != null && ((GGJTile)curr).ID == TileID.End)
-                    return;
-
-                if (curr != null && ((GGJTile)curr).ID == TileID.Start)
-                    return;                
-
-                Level.Instance.UpdateBuildingCell(front.FromWorldToCellPosition(), false);   
-
-                var tower = TowerResources.Instance.GetTower(_towerInventory[selectedTowerIndex]);
-
-                tower.gameObject.Create(front.FromWorldToCellPosition().FromCellToWorldPosition(), Level.Instance.transform);
+                turretMenu.enabled = !isMenuActive;
+                isMenuActive = !isMenuActive;
+                           
             }
         }
+
+        void CreateTower(int selectedTowerIndex) {
+
+            var front =
+              avatar.Transform.position +
+              isoRenderer.Direction * _buildRange;
+
+            var curr = Level.Instance.Tilemap.GetTile(
+                     front.FromWorldToCellPosition());
+
+            if (curr != null && ((GGJTile)curr).ID == TileID.Building)
+                return;
+
+            if (curr != null && ((GGJTile)curr).ID == TileID.Character)
+                return;
+
+            if (curr != null && ((GGJTile)curr).ID == TileID.End)
+                return;
+
+            if (curr != null && ((GGJTile)curr).ID == TileID.Start)
+                return;
+
+            Level.Instance.UpdateBuildingCell(front.FromWorldToCellPosition(), false);
+
+            var tower = TowerResources.Instance.GetTower(_towerInventory[selectedTowerIndex]);
+
+            tower.gameObject.Create(front.FromWorldToCellPosition().FromCellToWorldPosition(), Level.Instance.transform);
+
+        }
+
+        void CreateShooting() { CreateTower(0); }
+        void CreateShield() { CreateTower(1); }
     }
 }
