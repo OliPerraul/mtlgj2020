da793e2526dbc84ca58487c3edc470a020c61f96
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2020-02-02 13:22:13 -0500

    asd

--- MtlGj/Assets/Game/OcclusionSilhouette.cs
+++ MtlGj/Assets/Game/OcclusionSilhouette.cs
@@ -10,14 +10,14 @@ namespace MTLGJ
         //[SerializeField]
         //private SpriteRenderer spr;
 
-        [SerializeField]
-        private SpriteRenderer _sprite;
+        //[SerializeField]
+        //private SpriteRenderer _sprite;
 
-        [SerializeField]
-        private Material _occlusionMaterial;
+        //[SerializeField]
+        //private Material _occlusionMaterial;
     
-        [SerializeField]
-        private Material _normalMaterial;
+        //[SerializeField]
+        //private Material _normalMaterial;
 
         //[SerializeField]
         //private Character _character;
@@ -26,33 +26,33 @@ namespace MTLGJ
 
         public void Awake()
         {
-            _sprite.material = _normalMaterial;
+            //_sprite.material = _normalMaterial;
         }
 
         public void OnTriggerStay2D(Collider2D collider)
         {
             //Debug.Log("asd");
 
-            if (collider.GetComponent<ColliderListener>() != null)
-                return;
+            //if (collider.GetComponent<ColliderListener>() != null)
+            //    return;
 
-            var tow = collider.GetComponentInParent<Tower>();
+            //var tow = collider.GetComponentInParent<Tower>();
 
-            if (tow == null)
-                return;
+            //if (tow == null)
+            //    return;
 
-            if (tow.Transform.position.y > transform.position.y)
-                return;
+            //if (tow.Transform.position.y > transform.position.y)
+            //    return;
 
-            _sprite.material = _occlusionMaterial;
+            //_sprite.material = _occlusionMaterial;
         }
 
         public void OnTriggerExit2D(Collider2D other)
         {
-            if (other.GetComponent<ColliderListener>() != null)
-                return;
+            //if (other.GetComponent<ColliderListener>() != null)
+            //    return;
 
-            _sprite.material = _normalMaterial;
+            //_sprite.material = _normalMaterial;
             
         }
 
