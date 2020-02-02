07ff4875e96d2085521fe04acf18fba33c6a2776
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2020-02-02 14:16:09 -0500

    save

--- MtlGj/Assets/Game/Enemies/EnemyStateMachine.cs
+++ MtlGj/Assets/Game/Enemies/EnemyStateMachine.cs
@@ -379,7 +379,7 @@ namespace MTLGJ
                 return true;
             }
 
-            if ( (Enemy.Transform.position - _target.Transform.position).magnitude <= 2)
+            if ( (Enemy.Transform.position - _target.Transform.position).magnitude <= 1)
             {
                 StateMachine.TrySetState(EnemyStateID.InAttack, _target);
                 return true;
