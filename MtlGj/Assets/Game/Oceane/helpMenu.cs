c36a963b4081ecbd19e8a528a20adcf57342f3a7
LilOceane <57155432+LilOceane@users.noreply.github.com>
2020-02-02 13:10:18 -0500

    E

---
+++ MtlGj/Assets/Game/Oceane/helpMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helpMenu : MonoBehaviour
+{
+
+    public GameObject panel;
+    // Start is called before the first frame update
+    public void apparaitre() {
+        this.panel.SetActive(true);
+
+    }
+
+    public void disapear()
+    {
+        this.panel.SetActive(false);
+    }
+}
